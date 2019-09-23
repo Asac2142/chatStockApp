e621df6316394ce656673087bae55051117d4418
Alban Carvallo Andres Salomon <andres.alban@novaseguroslatam.com>
2019-09-23 15:54:45 -0500

    modified api.js

--- api/api.js
+++ api/api.js
@@ -1,62 +1,54 @@
 const axios = require('axios');
-const csvTpJSON = require('csvtojson');
+const csvToJSON = require('csvtojson');
+const homedir = require('os').homedir();
+const path = require('path');
 const fs = require('fs');
 
 const getStockInfo = async (stockCode) => {
+    const fileName = `stockInfo_${getRandomNumber()}.csv`;
+    const subFolder = `temp${getRandomNumber()}`;
+    const pathToFile = path.join(homedir, `/${subFolder}/`);
+    const fullPath = `${pathToFile}${fileName}`;
+    let quote;
+
     try {
-        //result = await axios(`https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=${stockCode}&apikey=demo`);
-        //let result = await axios(`https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=MSFT&apikey=demo`);                
-        // let result = await axios('https://stooq.com/q/l/?s=aapl.us&f=sd2t2ohlcv&h&e=csv');
-        
         await axios({
             method: 'GET',
             url: 'https://stooq.com/q/l/?s=aapl.us&f=sd2t2ohlcv&h&e=csv',
             responseType: 'stream'
         }).then((response) => {
-            response.data.pipe(fs.createWriteStream('/home/andres/Documents/aFile1.csv'));
-            csvTpJSON().fromFile('/home/andres/Documents/aFile1.csv').then((source) => {
-                console.log(source);
-            });
-        });
-        
-        // console.log(result);
-        //let stock = getLatestStock(result.data['Time Series (Daily)']);        
-        // stock = parseFloat(stock['4. close']).toFixed(2);
-        // return `${stockCode.toUpperCase()} quote is $${stock} per share`;
+            fs.mkdir(pathToFile, {recursive: true}, (err) => {
+                if (err) {
+                    throw err;
+                } else {
+                    response.data.pipe(fs.createWriteStream(fullPath));              
+                    parseFile(fullPath).then((res) => {
+                        quote = `${stockCode} quote is $${res} per share`;
+                        console.log(quote);                       
+                    });
+                }                             
+            });            
+        }); 
+
+        console.log(quote);
+
     } catch (error) {
         console.log(`Somenthig went wrong getting stock info: ${error}`);
     }
 };
 
-const getLatestStock = (data) => {
-    let year = getYear();
-    let month = getFullMonth();
-    let day = getDay();
-    let date = `${year}-${month}-${day}`;
-    
-    while (true) {
-        if (data[date] === undefined) {
-            date = `${year}-${month}-${day}`;
-            day -= 1;            
+const parseFile = async (thePath) => { 
+    let amount;
+    await csvToJSON().fromFile(thePath).then((source) => {
+        if (source) { 
+            amount = source[0].Close;
         }
-        else {
-            return data[date];
-        }
-    }
+    });
+    return amount;
 };
 
-const getYear = () => {
-    return new Date().getFullYear();
-};
-
-const getDay = () => {
-    let day = new Date().getDate().toString();
-    return day.length > 1 ? day : `0${day}`;
-};
-
-const getFullMonth = () => {
-    let month = (new Date().getMonth() + 1).toString();
-    return month.length > 1 ? month : `0${month}`;
+const getRandomNumber = () => {
+    return Math.floor((Math.random() * 1000) + 1);
 };
 
 module.exports.getStockInfo = getStockInfo;
